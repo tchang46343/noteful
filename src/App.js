26ded0ded758ce09f8f07dbc2dce73b39ce0f757
Terrance Chang <tchang46343@gmail.com>
2020-03-05 09:15:51 -0800

    Test ran sucessful

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import React from "react";
-import { Route, Switch } from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import HomePage from "./homePage/homePage";
 import PostNote from "./postNotes/postNote";
 import PostFolder from "./postFolder/postFolder";
 
-export default class Home extends React.Component {
+export default class App extends React.Component {
   render() {
     return (
       <div className="landingLayout">
-        <Switch>
-          <Route exact path="/" component={HomePage} />
-          <Route exact path="/note" component={PostNote} />
-          <Route exact path="/folder" component={PostFolder} />
-        </Switch>
+        <BrowserRouter>
+          <Switch>
+            <Route exact path="/" component={HomePage} />
+            <Route exact path="/note" component={PostNote} />
+            <Route exact path="/folder" component={PostFolder} />
+          </Switch>
+        </BrowserRouter>
       </div>
     );
   }
