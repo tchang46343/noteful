d07ea6f1c7da46910ee9d86ba3c4971530fe2c84
Terrance Chang <tchang46343@gmail.com>
2020-03-07 11:48:22 -0800

    Testing data endpoint call

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter, Route, Switch } from "react-router-dom";
 import HomePage from "./homePage/homePage";
 import PostNote from "./postNotes/postNote";
 import PostFolder from "./postFolder/postFolder";
-import ErrorBoundary from "./ErrorBoundary";
+//import ErrorBoundary from "./ErrorBoundary";
 
 export default class App extends React.Component {
   render() {
@@ -11,9 +11,7 @@ export default class App extends React.Component {
       <div className="landingLayout">
         <BrowserRouter>
           <Switch>
-            <ErrorBoundary>
-              <Route exact path="/" component={HomePage} />
-            </ErrorBoundary>
+            <Route exact path="/" component={HomePage} />
 
             <Route exact path="/note" component={PostNote} />
             <Route exact path="/folder" component={PostFolder} />
