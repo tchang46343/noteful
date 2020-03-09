45519e1fd290ab24225d41c7aedd9f63218153f9
Terrance Chang <tchang46343@gmail.com>
2020-03-09 08:01:40 -0700

    Added in PropTypes and displaying notes and folder Content. Connected Cloud server

--- src/config.js
+++ src/config.js
@@ -1,6 +1,8 @@
 module.exports = {
   PORT: process.env.PORT || 3000,
 
-  API_BASE_URL: process.env.REACT_APP_API_BASE_URL || "http://localhost:8000/"
-  //"https://shielded-tundra-61491.herokuapp.com/"
+  API_BASE_URL:
+    process.env.REACT_APP_API_BASE_URL ||
+    "https://shielded-tundra-61491.herokuapp.com/"
+  //"http://localhost:8000/"
 };
